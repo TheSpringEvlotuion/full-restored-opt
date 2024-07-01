7080c7dd336da902fdd2240c0f76355809031dd5
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-07-01 21:08:41 +0300

    update source/gameObjects/userInterface/HealthIcon.hx

--- source/gameObjects/userInterface/HealthIcon.hx
+++ source/gameObjects/userInterface/HealthIcon.hx
@@ -5,7 +5,6 @@ import flixel.FlxSprite;
 import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxAtlasFrames;
 import openfl.utils.Assets as OpenFlAssets;
-import sys.FileSystem;
 
 using StringTools;
 
@@ -67,7 +66,7 @@ class HealthIcon extends FlxSprite
 				trimmedCharacter = trimmedCharacter.substring(0, trimmedCharacter.indexOf('-'));
 
 			var iconPath = char;
-			while (!FileSystem.exists(Paths.getPath('images/icons/icon-' + iconPath + '.png', IMAGE)))
+			while (!OpenFlAssets.exists(Paths.getPath('images/icons/icon-' + iconPath + '.png', IMAGE)))
 			{
 				if (iconPath != trimmedCharacter)
 					iconPath = trimmedCharacter;
