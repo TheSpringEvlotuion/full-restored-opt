0c1ca66f8dd61cc9d55fa9b8e022d7ce680afe73
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-07-05 08:48:39 +0300

    update source/Init.hx

--- source/Init.hx
+++ source/Init.hx
@@ -353,7 +353,7 @@ class Init extends FlxState
 			0.5,
 			99.5
 		],
-		'Shaders' => [Checkmark, true, "Whether to load shaders or not",],
+		'Shaders' => [Checkmark, false, "Whether to load shaders or not",],
 		"Unfocus Pause" => [Checkmark, true, "Whether the game should pause when you unfocus"],
 		'Snow Enabled' => [Checkmark, true, "Whether to enable the snow on the Mountain stage"],
 		'Freezing Enabled' => [
