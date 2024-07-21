b39b059f8bf3fd0f1952fa38de70df5c0e15448d
Dxgamer7405 <95672691+Dxgamer7405@users.noreply.github.com>
2024-07-21 12:58:38 -0300

    use this one

--- source/lime/utils/Log.hx
+++ source/lime/utils/Log.hx
@@ -1,12 +1,6 @@
-/*package lime.utils;
+package lime.utils;
 
 import haxe.PosInfos;
-import sys.io.File;
-import sys.FileSystem;
-import openfl.Lib; // I FORGOR
-import mobile.utils.Asset2File;
-
-using StringTools; // AGAIN
 
 #if !lime_debug
 @:fileXml('tags="haxe,release"')
@@ -14,146 +8,143 @@ using StringTools; // AGAIN
 #end
 class Log
 {
-	public static var level:LogLevel;
-	public static var throwErrors:Bool = true;
+  public static var level:LogLevel;
+  public static var throwErrors:Bool = true;
 
-	public static function debug(message:Dynamic, ?info:PosInfos):Void
-	{
-		if (level >= LogLevel.DEBUG)
-		{
-			#if js
-			untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").debug("[" + info.className + "] " + message);
-			#else
-			println("[" + info.className + "] " + Std.string(message));
-			#end
-		}
-	}
+  public static function debug(message:Dynamic, ?info:PosInfos):Void
+  {
+    if (level >= LogLevel.DEBUG)
+    {
+      #if js
+      untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").debug("[" + info.className + "] " + message);
+      #else
+      println("[" + info.className + "] " + Std.string(message));
+      #end
+    }
+  }
 
-	public static function error(message:Dynamic, ?info:PosInfos):Void
-	{
-		if (level >= LogLevel.ERROR)
-		{
-			var message = "[" + info.className + "] ERROR: " + message;
+  public static function error(message:Dynamic, ?info:PosInfos):Void
+  {
+    if (level >= LogLevel.ERROR)
+    {
+      var message = "[" + info.className + "] ERROR: " + message;
 
-			if (throwErrors)
-			{
-			  if (!FileSystem.exists(Asset2File.getPath() + 'logs'))
-					FileSystem.createDirectory(Asset2File.getPath() + 'logs');
+      if (throwErrors)
+      {
+        #if webassembly
+        println(message);
+        #end
 
-				File.saveContent(Asset2File.getPath()
-					+ 'logs/'
-					+ Lib.application.meta.get('file')
-					+ '-'
-					+ Date.now().toString().replace(' ', '-').replace(':', "'")
-					+ '.log',
-					message
-					+ '\n');
+        #if (mobile && !macro)
+        @:privateAccess
+        funkin.util.logging.CrashHandler.logErrorMessage(message);
+        #end
 
-                                Lib.application.window.alert(message, 'Error!');
-				throw message;
-			}
-			else
-			{
-				#if js
-				untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").error(message);
-				#else
-				println(message);
-				#end
-			}
-		}
-	}
+        throw message;
+      }
+      else
+      {
+        #if js
+        untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").error(message);
+        #else
+        println(message);
+        #end
+      }
+    }
+  }
 
-	public static function info(message:Dynamic, ?info:PosInfos):Void
-	{
-		if (level >= LogLevel.INFO)
-		{
-			#if js
-			untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").info("[" + info.className + "] " + message);
-			#else
-			println("[" + info.className + "] " + Std.string(message));
-			#end
-		}
-	}
+  public static function info(message:Dynamic, ?info:PosInfos):Void
+  {
+    if (level >= LogLevel.INFO)
+    {
+      #if js
+      untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").info("[" + info.className + "] " + message);
+      #else
+      println("[" + info.className + "] " + Std.string(message));
+      #end
+    }
+  }
 
-	public static inline function print(message:Dynamic):Void
-	{
-		#if sys
-		Sys.print(Std.string(message));
-		#elseif flash
-		untyped __global__["trace"](Std.string(message));
-		#elseif js
-		untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").log(message);
-		#else
-		trace(message);
-		#end
-	}
+  public static inline function print(message:Dynamic):Void
+  {
+    #if sys
+    Sys.print(Std.string(message));
+    #elseif flash
+    untyped __global__["trace"](Std.string(message));
+    #elseif js
+    untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").log(message);
+    #else
+    trace(message);
+    #end
+  }
 
-	public static inline function println(message:Dynamic):Void
-	{
-		#if sys
-		Sys.println(Std.string(message));
-		#elseif flash
-		untyped __global__["trace"](Std.string(message));
-		#elseif js
-		untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").log(message);
-		#else
-		trace(Std.string(message));
-		#end
-	}
+  public static inline function println(message:Dynamic):Void
+  {
+    #if sys
+    Sys.println(Std.string(message));
+    #elseif flash
+    untyped __global__["trace"](Std.string(message));
+    #elseif js
+    untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").log(message);
+    #else
+    trace(Std.string(message));
+    #end
+  }
 
-	public static function verbose(message:Dynamic, ?info:PosInfos):Void
-	{
-		if (level >= LogLevel.VERBOSE)
-		{
-			println("[" + info.className + "] " + message);
-		}
-	}
+  public static function verbose(message:Dynamic, ?info:PosInfos):Void
+  {
+    if (level >= LogLevel.VERBOSE)
+    {
+      println("[" + info.className + "] " + message);
+    }
+  }
 
-	public static function warn(message:Dynamic, ?info:PosInfos):Void
-	{
-		if (level >= LogLevel.WARN)
-		{
-			#if js
-			untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").warn("[" + info.className + "] WARNING: " + message);
-			#else
-			println("[" + info.className + "] WARNING: " + Std.string(message));
-			#end
-		}
-	}
+  public static function warn(message:Dynamic, ?info:PosInfos):Void
+  {
+    if (level >= LogLevel.WARN)
+    {
+      #if js
+      untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").warn("[" + info.className + "] WARNING: " + message);
+      #else
+      println("[" + info.className + "] WARNING: " + Std.string(message));
+      #end
+    }
+  }
 
-	private static function __init__():Void
-	{
-		#if no_traces
-		level = NONE;
-		#elseif verbose
-		level = VERBOSE;
-		#else
-		#if sys
-		var args = Sys.args();
-		if (args.indexOf("-v") > -1 || args.indexOf("-verbose") > -1)
-		{
-			level = VERBOSE;
-		}
-		else
-		#end
-		{
-			#if debug
-			level = DEBUG;
-			#else
-			level = INFO;
-			#end
-		}
-		#end
+  private static function __init__():Void
+  {
+    #if no_traces
+    level = NONE;
+    #elseif verbose
+    level = VERBOSE;
+    #else
+    #if sys
+    var args = Sys.args();
+    if (args.indexOf("-v") > -1 || args.indexOf("-verbose") > -1)
+    {
+      level = VERBOSE;
+    }
+    else
+    #end
+    {
+      #if debug
+      level = DEBUG;
+      #else
+      level = INFO;
+      #end
+    }
+    #end
 
-		#if js
-		if (untyped #if haxe4 js.Syntax.code #else __js__ #end ("typeof console") == "undefined")
-		{
-			untyped #if haxe4 js.Syntax.code #else __js__ #end ("console = {}");
-		}
-		if (untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").log == null)
-		{
-			untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").log = function() {};
-		}
-		#end
-	}
-}*/
+    #if js
+    if (untyped #if haxe4 js.Syntax.code #else __js__ #end ("typeof console") == "undefined")
+    {
+      untyped #if haxe4 js.Syntax.code #else __js__ #end ("console = {}");
+    }
+    if (untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").log == null)
+    {
+      untyped #if haxe4 js.Syntax.code #else __js__ #end ("console").log = function() {
+      };
+    }
+    #end
+  }
+}
